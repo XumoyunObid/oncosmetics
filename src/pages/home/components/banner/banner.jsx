1ddbc54fd3477510e7c48879e64dc07b366ca9d7
Xumoyun <obidjonov06122005@gmail.com>
2024-01-31 10:35:55 +0500

    home

--- src/pages/home/components/banner/banner.jsx
+++ src/pages/home/components/banner/banner.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import "./banner.css"
 
 export const Banner = () => {
-  return <div>Banner</div>;
+  return <section className="showcase">
+    <div>
+      <h1>Discover summer promotions</h1>
+      <p>Free sample and free shipping when you spend $50</p>
+      <div>
+        <button>Skincare</button>
+        <button>Shop All</button>
+      </div>
+    </div>
+  </section>;
 };
