431e3c485cb16287f2f331ac47f9a3927cf33b1a
Xumoyun <obidjonov06122005@gmail.com>
2024-02-01 11:44:10 +0500

    tyuioiuyt

--- src/data/data.js
+++ src/data/data.js
@@ -34,5 +34,47 @@ export const data = [
     url: img5,
     title: "Post-Shave Lotion",
     price: "$54.00"
-  }
+  },
+  {
+    id: 1,
+    url: img1,
+    title: "Facial Balancing Gel",
+    price: "$45.00 - $99.00"
+  },
+  {
+    id: 2,
+    url: img2,
+    title: "Post-Shave Lotion",
+    price: "$54.00"
+  },
+  {
+    id: 3,
+    url: img3,
+    title: "Facial Balancing Gel",
+    price: "$25.00"
+  },
+  {
+    id: 4,
+    url: img4,
+    title: "Eye Cream",
+    price: "$25.00 $19.00"
+  },
+  {
+    id: 5,
+    url: img5,
+    title: "Post-Shave Lotion",
+    price: "$54.00"
+  },
+  {
+    id: 1,
+    url: img1,
+    title: "Facial Balancing Gel",
+    price: "$45.00 - $99.00"
+  },
+  {
+    id: 2,
+    url: img2,
+    title: "Post-Shave Lotion",
+    price: "$54.00"
+  },
 ]
