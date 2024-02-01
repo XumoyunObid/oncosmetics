22f773c987fbedc9423c6f2cace01ab25f8a2a1b
Nusratillo <nxursanjonov888@gmail.com>
2024-02-01 09:58:28 +0500

    nimadur

---
+++ src/pages/blogs-page/blogsPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './blogsPage.css'
+// import img
+import img1 from '../../assets/blogsPage-img/blogs-img1.svg'
+import img2 from '../../assets/blogsPage-img/blogs-img2.svg'
+import img3 from '../../assets/blogsPage-img/blogs-img3.svg'
+import img4 from '../../assets/blogsPage-img/blogs-img4.svg'
+import img5 from '../../assets/blogsPage-img/blogs-img5.svg'
+import img6 from '../../assets/blogsPage-img/blogs-img6.svg'
+import img7 from '../../assets/blogsPage-img/blogs-img7.svg'
+import img8 from '../../assets/blogsPage-img/blogs-img8.svg'
+// import img end
+
+const EightCards = ({img, title, text1, text2}) => {
+  return (
+    <div>blogsPage</div>
+  )
+}
+
+
+export const BlogsPage = () => {
+  try {
+    return (
+        <section className='blogsPage-container'>
+
+        </section>
+    )
+  } catch (e) {console.log(e);}
+}
